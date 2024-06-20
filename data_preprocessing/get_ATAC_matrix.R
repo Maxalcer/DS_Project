a7d33fafbb4ccb7c22939770e8e413d36613b5d6
maxa55 <maxa55@mi.fu-berlin.de>
2024-06-20 23:54:18 +0200

    get atac matrix opt

--- data_preprocessing/get_ATAC_matrix.R
+++ data_preprocessing/get_ATAC_matrix.R
@@ -11,15 +11,17 @@ files <- read.table(paste(path, "md5.txt", sep = ""))$V2
 
 genes <- genes(TxDb.Mmusculus.UCSC.mm10.knownGene)
 
-total_df <- tibble("gene_symbol" = character())
+all_genes <- c()
 
 for(file in files){
-  fragments <- read.delim(paste(path, file, sep = ""), header = FALSE)
+  fragments <- read_tsv(paste(path, file, sep = ""), 
+                        col_names = FALSE,
+                        num_threads = 4)
   
-  atac_Granges <- GRanges(seqnames = atac_data$V1,
-                          ranges = IRanges(start = atac_data$V2, 
-                                           end = atac_data$V3),
-                          sample = atac_data$V4)
+  atac_Granges <- GRanges(seqnames = fragments$X1,
+                          ranges = IRanges(start = fragments$X2, 
+                                           end = fragments$X3),
+                          sample = fragments$X4)
   rm(fragments)
   
   nearest_genes <- nearest(atac_Granges, genes)
@@ -34,17 +36,47 @@ for(file in files){
   
   atac_Granges$gene_symbol <- gene_symbols
   
-  atac_df <- as.data.frame(atac_Granges)
+  atac_df <- as_tibble(atac_Granges)
+  
+  rm(atac_Granges)
   
   atac_df <- atac_df %>%
     group_by(gene_symbol, sample) %>%
     summarise(count = n()) %>%
     ungroup()
   
-  atac_df <- pivot_wider(atac_df, names_from = sample, values_from = count, values_fill = list(count = 0))
-  atac_df <- column_to_rownames(atac_df, var = "gene_symbol")
+  atac_df <- pivot_wider(atac_df, names_from = sample, values_from = count, values_fill = list(count = 0)) %>%
+    drop_na(gene_symbol) %>%
+    mutate(gene_symbol = as.character(gene_symbol)) %>%
+    column_to_rownames(var = "gene_symbol")
   
-  total_df <- full_join(total_df, atac_df) %>% replace(is.na(.), 0)
+  all_genes <- unique(c(all_genes, row.names(atac_df)))
+  
+  atac_matrix <- as(atac_df, "sparseMatrix")
+  rm(atac_df)
+  if(file == files[1]){
+    total_matrix <- atac_matrix
+  }
+  else{
+    temp1 <- Matrix(0, 
+                    nrow = length(all_genes), 
+                    ncol = ncol(atac_matrix),
+                    dimnames = list(all_genes, colnames(atac_matrix)),
+                    sparse = TRUE)
+    
+    temp2 <- Matrix(0, 
+                    nrow = length(all_genes), 
+                    ncol = ncol(total_matrix),
+                    dimnames = list(all_genes, colnames(total_matrix)),
+                    sparse = TRUE)
+    
+    temp1[rownames(atac_matrix),] <- atac_matrix
+    rm(atac_matrix)
+    temp2[rownames(total_matrix),] <- total_matrix
+    
+    total_matrix <- cbind(temp1, temp2)
+    rm(temp1, temp2)
+  }
 }
 
-saveRDS(as(total_df, "sparseMatrix"), "..data/unprocessed/ATAC_counts.rds")
+saveRDS(total_matrix, "..data/unprocessed/ATAC_counts.rds")
